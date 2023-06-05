d914e75e8f09bdabd26269cad263c75a7df7ad40
Guillaume <g.fortin@sollum.tech>
2023-06-05 19:36:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* ' <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The cavalcade website is under reconstruction for the 2023 edition. New information coming soon!
         </p>
-        <a
+        {/*<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React yoooooooooo
+        </a>*/}
       </header>
     </div>
   );
